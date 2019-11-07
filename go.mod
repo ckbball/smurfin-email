4f075b03567c98011c45ba2114e2510526216b8c
Caesar Ladion <caesar.ladion@gmail.com>
2019-11-07 14:27:02 -0800

    email function working

--- go.mod
+++ go.mod
@@ -1,1 +1,8 @@
 module github.com/ckbball/smurfin-email
+
+require (
+	github.com/Shopify/sarama v1.24.1 // indirect
+	github.com/ThreeDotsLabs/watermill v1.0.1 // indirect
+	github.com/ThreeDotsLabs/watermill-kafka/v2 v2.0.1 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,20 +1,63 @@
 package main
 
 import (
-  "bytes"
-  "context"
-  "encoding/gob"
-  "github.com/Shopify/sarama"
-  "github.com/ThreeDotsLabs/watermill"
-  "github.com/ThreeDotsLabs/watermill-kafka/v2/pkg/kafka"
-  "github.com/ThreeDotsLabs/watermill/message"
-  catalogProto "github.com/ckbball/smurfin-catalog/proto/catalog"
-  pb "github.com/ckbball/smurfin-checkout/proto/checkout"
-  "time"
+  //"bytes"
+  //"context"
+  //"encoding/gob"
+  "fmt"
+  /*
+     "github.com/Shopify/sarama"
+     "github.com/ThreeDotsLabs/watermill"
+     "github.com/ThreeDotsLabs/watermill-kafka/v2/pkg/kafka"
+     "github.com/ThreeDotsLabs/watermill/message"
+     catalogProto "github.com/ckbball/smurfin-catalog/proto/catalog"
+     pb "github.com/ckbball/smurfin-checkout/proto/checkout"
+  */
+  "github.com/subosito/gotenv"
+  "net/smtp"
+  "os"
+  //"time"
 )
+
+func init() {
+  gotenv.Load()
+}
 
 func main() {
   // kafka subscriber
   // user-service client
-  //
+  // run the subscriber
+  //  then it calls email function???
+  // Sender data.
+  from := os.Getenv("SENDER_EMAIL")
+  password := os.Getenv("SENDER_PASSWORD")
+  // Receiver email address.
+  to := []string{
+    "blah1@gmail.com",
+    "blah2@gmail.com",
+  }
+  // smtp server configuration.
+  smtpServer := smtpServer{host: "smtp.gmail.com", port: "587"}
+  // Message.
+  message := []byte("This is a really unimaginative message, I know.")
+  // Authentication.
+  auth := smtp.PlainAuth("", from, password, smtpServer.host)
+  // Sending email.
+  err := smtp.SendMail(smtpServer.serverName(), auth, from, to, message)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  fmt.Println("Email Sent!")
+}
+
+// smtpServer data to smtp server
+type smtpServer struct {
+  host string
+  port string
+}
+
+// serverName URI to smtp server
+func (s *smtpServer) serverName() string {
+  return s.host + ":" + s.port
 }
